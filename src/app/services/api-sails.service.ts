14ae82e02e6297c4e9f3d3b6355741df3af23e48
tecladozurdo <efrainup@hotmail.es>
2018-07-17 22:47:29 -0500

    fix: identacion

--- src/app/services/api-sails.service.ts
+++ src/app/services/api-sails.service.ts
@@ -12,9 +12,9 @@ export class ApiSailsService {
   constructor(public httpclient: HttpClient) {
     this.apiAddres = 'http://localhost:1337/tablacontacto/'
 
-   }
-   getCliente(): Observable<Array<Cliente>>{
-   return this.httpclient.get<Array<Cliente>>(this.apiAddres)
-   }  
-    }
+  }
+  getCliente(): Observable<Array<Cliente>>{
+    return this.httpclient.get<Array<Cliente>>(this.apiAddres)
+  }
   
+}
